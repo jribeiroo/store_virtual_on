84ed0249f49b36d237ed6c758cd8823833ceddcc
jribeiroo <nicolas.jnr10@outlook.com>
2022-05-10 01:21:07 -0300

    feat(firebase): cria e testa conexao com banco

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
+
+  FirebaseFirestore.instance.collection('teste').add({'teste': 'teste'});
+
 }
 
 class MyApp extends StatelessWidget {
