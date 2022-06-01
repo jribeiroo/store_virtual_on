72edb44622cd61f2315de9574ca1b206813a437f
jribeiroo <nicolas.jnr10@outlook.com>
2022-06-01 10:05:48 -0300

    ajuste em Indentação

--- lib/common/custom_drawer/custom_drawer.dart
+++ lib/common/custom_drawer/custom_drawer.dart
@@ -1,24 +1,22 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
 import 'drawer_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
-  const CustomDrawer({ Key? key }) : super(key: key);
+  const CustomDrawer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        // ignore: prefer_const_literals_to_create_immutables
-        children: [
-          DrawerTile(iconData: Icons.home, title: 'inicio', page: 0),
-          DrawerTile(iconData: Icons.list, title: 'produtos', page: 1),
-          DrawerTile(iconData: Icons.playlist_add_check, title: 'Meus pedidos' , page: 2),
-          DrawerTile(iconData: Icons.location_on, title: 'lojas' , page: 3),
-        ],
-      )
-    );
+        child: ListView(
+      children: [
+        DrawerTile(iconData: Icons.home, title: 'inicio', page: 0),
+        DrawerTile(iconData: Icons.list, title: 'produtos', page: 1),
+        DrawerTile(iconData: Icons.playlist_add_check, title: 'Meus pedidos', page: 2),
+        DrawerTile(iconData: Icons.location_on, title: 'lojas', page: 3),
+      ],
+    ));
   }
 }
