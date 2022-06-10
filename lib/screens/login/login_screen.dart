6616beffc8ba1236481dbbaa4431945ba3351aac
jribeiroo <nicolas.jnr10@outlook.com>
2022-06-10 01:44:54 -0300

    add expressão regular para validação de campo e globalKey

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
+import '../../helpers/validators.dart';
+
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+ LoginScreen({Key? key}) : super(key: key);
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -13,54 +20,67 @@ class LoginScreen extends StatelessWidget {
         body: Center(
           child: Card(
             margin: const EdgeInsets.symmetric(horizontal: 16),
-            child: ListView(
-              padding: const EdgeInsets.all(16),
-              shrinkWrap: true,
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'E-mail',
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                  autocorrect: false,
-                  validator: (email) {
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Senha',
-                  ),
-                  autocorrect: false,
-                  obscureText: true,
-                  validator: (pass) {
-                    if (pass!.isEmpty || pass.length < 6) {
-                      return 'Senha inválida';
+            child: Form(
+              key: _formKey,
+              child: ListView(
+                padding: const EdgeInsets.all(16),
+                shrinkWrap: true,
+                children: [
+                  TextFormField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      hintText: 'E-mail',
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    autocorrect: false,
+                    validator: (email) {
+                      if (!emailValid(email!)) {
+                        return 'E-mail inválido';
+                      }
                       return null;
-                    }
-                  },
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: FlatButton(
-                    onPressed: () {},
-                    padding: EdgeInsets.zero,
-                    child: const Text('Esqueci minha senha' , style: TextStyle(color: Color.fromARGB(255, 68, 68, 68)),),
+                    },
                   ),
-                ),
-                const SizedBox(height: 16),
-                SizedBox(
-                  height: 44,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: const Text(
-                      'Entrar',
-                      style: TextStyle(fontSize: 18),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    controller: passwordController,
+                    decoration: const InputDecoration(
+                      hintText: 'Senha',
+                    ),
+                    autocorrect: false,
+                    obscureText: true,
+                    validator: (pass) {
+                      if (pass!.isEmpty || pass.length < 6) {
+                        return 'Senha inválida';
+                      }else{
+                        return null;
+                      }
+                    },
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: FlatButton(
+                      onPressed: () {},
+                      padding: EdgeInsets.zero,
+                      child: const Text('Esqueci minha senha' , style: TextStyle(color: Color.fromARGB(255, 68, 68, 68)),),
                     ),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 16),
+                  SizedBox(
+                    height: 44,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if(_formKey.currentState!.validate()){
+                          
+                        }
+                      },
+                      child: const Text(
+                        'Entrar',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ));
