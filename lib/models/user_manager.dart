b25eaf83ea490204896eee620d02077cece1dbf1
jribeiroo <nicolas.jnr10@outlook.com>
2022-06-27 22:53:38 -0300

    erro ainda no snackBar

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -3,20 +3,22 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:store_virtual_pro/models/user_person.dart';
 
+import '../helpers/firebase_erros.dart';
+
 class UserManager {
   final FirebaseAuth auth = FirebaseAuth.instance;
 
-  Future<void> signIn(UserPerson userPerson) async {
-    try { //login com sucesso
+  Future<void> signIn({UserPerson? userPerson, Function? onFail, Function? onSuccess}) async {
+    //onFail para quando falhar chamar para tela de login com erro aplicado(callBack)
+    try {
+      //login com sucesso
       final UserCredential credential = await auth.signInWithEmailAndPassword(
-          email: userPerson.email, password: userPerson.password);
-      if (kDebugMode) {
-        print(credential.user?.uid);
-      } 
-    } on PlatformException catch (e) { //login com erro
-      if (kDebugMode) {
-        print(e);  
-      }
+          email: userPerson!.email, password: userPerson.password);
+
+      onSuccess!();
+    } on PlatformException catch (e) {
+      //login com erro
+      onFail!(getErrorString(e.code));
     }
   }
 }
